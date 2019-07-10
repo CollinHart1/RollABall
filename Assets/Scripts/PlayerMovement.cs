120a00b1e2aa96ba7ad360ff7c4f3212446091f0
CollinHart1 <collinhart@knights.ucf.edu>
2019-07-09 21:09:17 -0400

    Real Final

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text scoreText;
+    public Text livesText;
+    public Text winText;
+    public Text loseText;
+    public GameObject player;
+
+
+    private Rigidbody rb;
+    private int count;
+    private int lives;
+    private int score;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+            count = 0;
+            score = 0;
+            lives = 3;
+            SetCountText ();
+            SetScoreText ();
+            SetLivesText ();
+
+            winText.text = "";
+            loseText.text = "";
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    void LateUpdate()
+    {
+
+        float moveHorizantal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizantal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            score = score + 1;
+            SetCountText ();
+            SetScoreText ();
+        }
+
+        if(other.gameObject.CompareTag("enemy"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            score = score - 1;
+            lives = lives - 1;
+            SetScoreText ();
+            SetCountText ();
+            SetLivesText ();
+        }
+        if(lives <= 0)
+        {
+            Destroy(player);
+            loseText.text = "You lose";
+        }
+    }
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+    }
+
+    void SetLivesText ()
+    {
+        livesText.text = "Lives: " + lives.ToString ();
+    }
+
+    void SetScoreText ()
+    {
+        scoreText.text = "Score: " + score.ToString ();
+        if (score >= 23)
+        {
+            winText.text = "You Win";
+        }
+    }
+}
---
+++ Assets/Scripts/obstical.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstical : MonoBehaviour
+{
+    public Transform box;
+    void Start(){
+        box.parent = transform;
+    }
+
+    void Update()
+    {
+        transform.eulerAngles += new Vector3(0, 100 *Time.deltaTime, 0);
+    }
+}
